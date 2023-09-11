89aef19147e7bd204686b714e3fe25bebe91f698
Martin Schubert <30735893+mfschubert@users.noreply.github.com>
2023-09-11 12:25:56 -0700

    Version updated from 0.0.0 to 0.1.0

--- src/gifcm/__init__.py
+++ src/gifcm/__init__.py
@@ -1,6 +1,6 @@
 """gifcm - Easy gif animation of matplotlib figures."""
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 __author__ = "Martin Schubert <mfschubert@gmail.com>"
 __all__ = ["AnimatedFigure", "Frame"]
 
